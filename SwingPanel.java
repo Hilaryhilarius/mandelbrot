6b18487357f014de38d4905937b8a5243f193afc
Hilaryhilarius <61486184+Hilaryhilarius@users.noreply.github.com>
2020-04-16 03:41:28 -0700

    still trying

---
+++ SwingPanel.java
@@ -0,0 +1,29 @@
+package com.eonsahead.swing;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.GeneralPath;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SwingPanel extends JPanel{
+
+    private Color color = Color.red;
+
+    public Color getColor() {
+        return this.color;
+    } // getColor()
+
+    public void setColor(Color c) {
+        this.color = c;
+    } // setColor( Color )
+
+
+} // SwingPanel
